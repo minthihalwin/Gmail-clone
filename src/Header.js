2d25e2ac9af17728a49e251826de77640edb92b2
minthihalwin <minthihalwin.dev@gmail.com>
2023-02-01 12:23:25 +1300

    Completed Header Design

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,41 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Avatar, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import AppsIcon from "@mui/icons-material/Apps";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
 function Header() {
   return (
-    <div className='header'>
-        <h1>This is Header</h1>
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://cdn.vox-cdn.com/thumbor/jJ_w_lWMMvGKoaLp_zaEXJpyZ9c=/0x0:1320x880/1400x788/filters:focal(660x440:661x441)/cdn.vox-cdn.com/uploads/chorus_asset/file/21939811/newgmaillogo.jpg"
+          alt=""
+        />
+      </div>
+      <div className="header__middle">
+        <SearchIcon />
+        <input type="text" placeholder="Search mail" />
+        <ArrowDropDownIcon className="header__inputCaret" />
+      </div>
+
+      <div className="header__right">
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+            <NotificationsIcon />
+        </IconButton>
+        <Avatar />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
