f879a6c37d0de6016411347ff358a30ee9365e22
minthihalwin <minthihalwin.dev@gmail.com>
2023-02-02 09:50:08 +1300

    Completed EmialList Setion Desgin

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import Sidebar from './Sidebar';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Mail from "./Mail";
+import EmailList from "./EmailList";
 
 function App() {
   return (
-   <div className='app'>
-    <Header />
-    <Sidebar />
-    
-   </div>
+    <div className="app">
+      <Header />
+      <div className="app__body">
+        <Sidebar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/mail" element={<Mail />} />
+            <Route path="/" element={<EmailList />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div>
   );
 }
 
---
+++ src/EmailRow.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './EmailRow.css'
+
+function EmailRow({id, title, subject, description}) {
+  return (
+    <div className='emailRow'>
+    
+    
+    </div>
+  )
+}
+
+export default EmailRow
